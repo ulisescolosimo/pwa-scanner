46fee1d51f02187a7abed5a8a88c930dbdef412b
Ulises <colosimo101@gmail.com>
2025-11-21 14:02:01 -0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,15 @@ export const metadata: Metadata = {
     capable: true,
     statusBarStyle: "black-translucent",
     title: "PWA Scanner"
+  },
+  icons: {
+    icon: [
+      { url: "/icons/android/android-launchericon-192-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icons/android/android-launchericon-512-512.png", sizes: "512x512", type: "image/png" }
+    ],
+    apple: [
+      { url: "/icons/ios/180.png", sizes: "180x180", type: "image/png" }
+    ]
   }
 }
 
